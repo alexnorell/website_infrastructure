c6f174db66fc310fa418de20f14ef27b09f69f9d
Alex Norell <alex.norell@gmail.com>
2020-08-22 14:12:06 -0700

    Pull Certificate out to separate file
    Clean up cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,4 +1,7 @@
 resource "aws_cloudfront_distribution" "domain_distribution" {
+  enabled             = true
+  default_root_object = "index.html"
+  aliases             = [local.full_domain]
   origin {
     custom_origin_config {
       http_port              = "80"
@@ -6,15 +9,18 @@ resource "aws_cloudfront_distribution" "domain_distribution" {
       origin_protocol_policy = "http-only"
       origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
-
-    // Endpoint
     domain_name = aws_s3_bucket.website.website_endpoint
     origin_id   = local.full_domain
   }
-
-  enabled             = true
-  default_root_object = "index.html"
-
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.www.arn
+    ssl_support_method  = "sni-only"
+  }
   default_cache_behavior {
     viewer_protocol_policy = "redirect-to-https"
     compress               = true
@@ -24,27 +30,11 @@ resource "aws_cloudfront_distribution" "domain_distribution" {
     min_ttl                = 0
     default_ttl            = 86400
     max_ttl                = 31536000
-
     forwarded_values {
       query_string = false
       cookies {
         forward = "none"
       }
     }
-  }
-
-  // Alias
-  aliases = [local.full_domain]
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  // Certification goes here
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.www.arn
-    ssl_support_method  = "sni-only"
   }
 }
--- domain.tf
+++ domain.tf
@@ -27,37 +27,3 @@ resource "aws_route53_record" "www" {
     evaluate_target_health = true
   }
 }
-
-resource "aws_route53_record" "www_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.www.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.domain.zone_id
-}
-
-
-resource "aws_acm_certificate" "www" {
-  provider          = aws.us_east_1
-  domain_name       = local.full_domain
-  validation_method = "DNS"
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_acm_certificate_validation" "www" {
-  provider                = aws.us_east_1
-  certificate_arn         = aws_acm_certificate.www.arn
-  validation_record_fqdns = [for record in aws_route53_record.www_validation : record.fqdn]
-}
-
